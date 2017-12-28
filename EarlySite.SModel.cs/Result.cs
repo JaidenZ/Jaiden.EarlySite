1f1a33045c0934f27b748ef44ddfbf934dc94d3b
赵浩君 <272665534@qq.com>
2017-12-28 10:02:02 +0800

    feat:增加控制器异常处理

---
+++ EarlySite.SModel.cs/Result.cs
@@ -0,0 +1,104 @@
+﻿namespace EarlySite.SModel
+{
+    using System;
+    using System.Data;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 返回结果
+    /// </summary>
+    public class Result
+    {
+
+        /// <summary>
+        /// 结果状态
+        /// </summary>
+        public bool Status { get; set; }
+
+        /// <summary>
+        /// 结果代码
+        /// </summary>
+        public string StatusCode { get; set; }
+
+        /// <summary>
+        /// 结果文本
+        /// </summary>
+        public string Message { get; set; }
+
+
+        /// <summary>
+        /// 失败时候调用
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Result Fail(string message)
+        {
+            return new Result { Status = false, Message = message };
+        }
+
+        /// <summary>
+        /// 失败时调用
+        /// </summary>
+        /// <param name="statusCode">错误异常时的代码</param>
+        /// <param name="message">结果文本</param>
+        /// <returns></returns>
+        public static Result Fail(string statusCode, string message)
+        {
+            var result = Fail(message);
+            result.StatusCode = statusCode;
+            return result;
+        }
+
+        /// <summary>
+        /// 成功时候调用
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Result Success(string message)
+        {
+            return new Result { Status = true, Message = message };
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        /// <summary>
+        /// 返回数据集
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// 失败时候调用
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Result<T> Fail(string message)
+        {
+            return new Result<T> { Status = false, Message = message };
+        }
+
+        /// <summary>
+        /// 失败时调用
+        /// </summary>
+        /// <param name="statusCode">错误异常时的代码</param>
+        /// <param name="message">结果文本</param>
+        /// <returns></returns>
+        public static Result<T> Fail(string statusCode, string message)
+        {
+            var result = Fail(message);
+            result.StatusCode = statusCode;
+            return result;
+        }
+
+        /// <summary>
+        /// 成功时候调用
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Result<T> Success(string message)
+        {
+            return new Result<T> { Status = true, Message = message };
+        }
+    }
+}
---
+++ EarlySite.Web/Controllers/BaseController.cs
@@ -0,0 +1,62 @@
+﻿namespace EarlySite.Web.Controllers
+{
+    using EarlySite.SModel;
+    using System;
+    using System.Web.Mvc;
+
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+            ViewBag.Title = "EarlySite";
+        }
+
+
+        /// <summary>
+        /// 重写调用方法前操作
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //验证登录信息或其他
+
+            base.OnActionExecuting(filterContext);
+        }
+
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+
+
+
+            base.HandleUnknownAction(actionName);
+        }
+
+        /// <summary>
+        /// 重写异常
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Exception exception = filterContext.Exception;
+            Result<Exception> exceptionmodel = new Result<Exception>()
+            {
+                Data = exception,
+                Message = exception.Message,
+                Status = false
+            };
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                filterContext.Result = Json(exceptionmodel, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                filterContext.Result = View("Error", exceptionmodel);
+            }
+
+            filterContext.ExceptionHandled = true;
+            //base.OnException(filterContext);
+        }
+        
+    }
+}
