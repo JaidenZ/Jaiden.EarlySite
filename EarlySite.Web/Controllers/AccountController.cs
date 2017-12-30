c445c479c170d212f69d7172ceaa939324abbeed
Haojun.Zhao <haojun.zhao@icloud.com>
2017-12-30 14:45:17 +0800

    faeture:增加账户界面

---
+++ EarlySite.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EarlySite.Web.Controllers
+{
+    public class AccountController : BaseController
+    {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+
+
+            return View();
+        }
+
+    }
+}
---
+++ EarlySite.Web/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EarlySite.Web.Controllers
+{
+    public class ProfileController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Titile = "EarlySite | Profile";
+
+            return View();
+        }
+    }
+}
