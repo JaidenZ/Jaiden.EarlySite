75224ecead4f58477c6bdc02d788f372d6d5d106
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-01-03 17:10:45 +0800

    style:更改导航栏展示

--- EarlySite.Web/Controllers/HomeController.cs
+++ EarlySite.Web/Controllers/HomeController.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace EarlySite.Web.Controllers
+﻿namespace EarlySite.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using EarlySite.SModel;
+
     public class HomeController : BaseController
     {
         public ActionResult Index()
         {
+            Account account = new Account();
+            account.NickName = "PandaTV";
+            ViewBag.Account = account;
+
 
 
             return View();
--- EarlySite.Web/Controllers/ProfileController.cs
+++ EarlySite.Web/Controllers/ProfileController.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace EarlySite.Web.Controllers
+﻿namespace EarlySite.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using EarlySite.SModel;
+
     public class ProfileController : BaseController
     {
         [HttpGet]
         public ActionResult Index()
         {
+            Account account = new Account();
+            account.NickName = "PandaTV";
+            ViewBag.Account = account;
+
             ViewBag.Titile = "EarlySite | Profile";
 
             return View();
