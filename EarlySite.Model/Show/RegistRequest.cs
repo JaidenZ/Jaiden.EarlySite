feb29fe57dc7b539347113c1c30da98520440392
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-01-04 15:43:10 +0800

    feature:增加注册请求模型

---
+++ EarlySite.Model/Show/RegistRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace EarlySite.Model.Show
+{
+    /// <summary>
+    /// 注册请求
+    /// </summary>
+    public class RegistRequest
+    {
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string SecurityCode { get; set; }
+
+
+    }
+}
