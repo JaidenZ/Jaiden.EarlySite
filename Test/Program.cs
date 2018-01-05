a1e2aa5fcd1ff7ab0f534ac825b87ece4e26ab6b
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-01-05 16:46:17 +0800

    test:测试项目

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,25 +13,31 @@ namespace Test
         static void Main(string[] args)
         {
 
-
-            FileInfo file = new FileInfo(@"D:\\TestCode\\Jaiden.EarlySite\\EarlySite.Web\\Themes\\Images");
-            FileStream stream = new FileStream(@"D:\\defaultHead.png", FileMode.Open);
-            byte[] headdata = new byte[stream.Length];
-            if (stream.CanRead)
-            {
-                int redabyte = 0;
-                do
-                {
-                    redabyte = stream.Read(headdata, 0, headdata.Length);
-
-                }
-                while (redabyte > 0);
-            }
-            stream.Close();
-
-            string base64 = Base64Engine.ToBase64String(headdata);
+            TestBase64();
 
 
+
+        }
+
+        public static void TestBase64()
+        {
+
+            //FileInfo file = new FileInfo(@"D:\\TestCode\\Jaiden.EarlySite\\EarlySite.Web\\Themes\\Images");
+            //FileStream stream = new FileStream(@"D:\\defaultHead.png", FileMode.Open);
+            //byte[] headdata = new byte[stream.Length];
+            //if (stream.CanRead)
+            //{
+            //    int redabyte = 0;
+            //    do
+            //    {
+            //        redabyte = stream.Read(headdata, 0, headdata.Length);
+
+            //    }
+            //    while (redabyte > 0);
+            //}
+            //stream.Close();
+
+            //string base64 = Base64Engine.ToBase64String(headdata);
         }
     }
 }
