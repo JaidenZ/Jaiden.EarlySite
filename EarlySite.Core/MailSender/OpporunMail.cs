0e2014e98d289db96ebdea5901d76012ba5b5f19
Haojun.Zhao <haojun.zhao@icloud.com>
2018-01-06 17:28:51 +0800

    feature:增加邮件发送组件与日志记录组件

---
+++ EarlySite.Core/MailSender/OpporunMail.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading;
+using EarlySite.Core.Utils;
+
+namespace EarlySite.Core.MailSender
+{
+    public class OpporunMail
+    {
+        private MailMessage mMailMessage;   //主要处理发送邮件的内容（如：收发人地址、标题、主体、图片等等）
+        private SmtpClient mSmtpClient; //主要处理用smtp方式发送此邮件的配置信息（如：邮件服务器、发送端口号、验证方式等等）
+        private int mSenderPort;   //发送邮件所用的端口号（htmp协议默认为25）
+        private string mSenderServerHost;    //发件箱的邮件服务器地址（IP形式或字符串形式均可）
+        private string mSenderPassword;    //发件箱的密码
+        private string mSenderUsername;   //发件箱的用户名（即@符号前面的字符串，例如：hello@163.com，用户名为：hello）
+        private bool mEnableSsl;    //是否对邮件内容进行socket层加密传输
+        private bool mEnablePwdAuthentication;  //是否对发件人邮箱进行密码验证
+
+        ///<summary>
+        /// 构造函数
+        ///</summary>
+        ///<param name="server">发件箱的邮件服务器地址</param>
+        ///<param name="toMail">收件人地址（可以是多个收件人）</param>
+        ///<param name="fromMail">发件人地址</param>
+        ///<param name="subject">邮件标题</param>
+        ///<param name="emailBody">邮件内容（可以以html格式进行设计）</param>
+        ///<param name="username">发件箱的用户名（即@符号前面的字符串，例如：hello@163.com，用户名为：hello）</param>
+        ///<param name="password">发件人邮箱密码</param>
+        ///<param name="port">发送邮件所用的端口号（htmp协议默认为25）</param>
+        ///<param name="sslEnable">true表示对邮件内容进行socket层加密传输，false表示不加密</param>
+        ///<param name="pwdCheckEnable">true表示对发件人邮箱进行密码验证，false表示不对发件人邮箱进行密码验证</param>
+        public OpporunMail(string server, IList<string> toMail, string fromMail, string subject, string emailBody, string username, string password, string port, bool sslEnable, bool pwdCheckEnable)
+        {
+            try
+            {
+
+                mMailMessage = new MailMessage();
+                for (int i = 0; i < toMail.Count; i++)
+                {
+                    mMailMessage.To.Add(toMail[i]);
+                }
+                mMailMessage.From = new MailAddress(fromMail);
+                mMailMessage.Subject = subject;
+                mMailMessage.Body = emailBody;
+                mMailMessage.IsBodyHtml = true;
+                mMailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+                mMailMessage.Priority = MailPriority.Normal;
+                this.mSenderServerHost = server;
+                this.mSenderUsername = username;
+                this.mSenderPassword = password;
+                this.mSenderPort = Convert.ToInt32(port);
+                this.mEnableSsl = sslEnable;
+                this.mEnablePwdAuthentication = pwdCheckEnable;
+            }
+            catch (Exception ex)
+            {
+                LoggerUtils.LogIn(LoggerUtils.ColectExceptionMessage(ex, "At function:CommonMail(Initial) .OpporunMail 62 rows"), LogType.ErrorLog);
+            }
+        }
+
+        ///<summary>
+        /// 添加附件
+        ///</summary>
+        ///<param name="attachmentsPath">附件的路径集合，以分号分隔</param>
+        public void AddAttachments(string attachmentsPath)
+        {
+            try
+            {
+                string[] path = attachmentsPath.Split(';'); //以什么符号分隔可以自定义
+                Attachment data;
+                ContentDisposition disposition;
+                for (int i = 0; i < path.Length; i++)
+                {
+                    data = new Attachment(path[i], MediaTypeNames.Application.Octet);
+                    disposition = data.ContentDisposition;
+                    disposition.CreationDate = File.GetCreationTime(path[i]);
+                    disposition.ModificationDate = File.GetLastWriteTime(path[i]);
+                    disposition.ReadDate = File.GetLastAccessTime(path[i]);
+                    mMailMessage.Attachments.Add(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggerUtils.LogIn(LoggerUtils.ColectExceptionMessage(ex, "At function:AddAttachments(string attachmentsPath) .OpporunMail 89 rows"), LogType.ErrorLog);
+            }
+        }
+
+        ///<summary>
+        /// 邮件的发送
+        ///</summary>
+        public void Send()
+        {
+            try
+            {
+                if (mMailMessage != null)
+                {
+                    mSmtpClient = new SmtpClient();
+                    mSmtpClient.Host = this.mSenderServerHost;
+                    mSmtpClient.Port = this.mSenderPort;
+                    mSmtpClient.UseDefaultCredentials = false;
+                    mSmtpClient.EnableSsl = this.mEnableSsl;
+                    if (this.mEnablePwdAuthentication)
+                    {
+                        System.Net.NetworkCredential nc = new System.Net.NetworkCredential(this.mSenderUsername, this.mSenderPassword);
+                        //mSmtpClient.Credentials = new System.Net.NetworkCredential(this.mSenderUsername, this.mSenderPassword);
+                        //NTLM: Secure Password Authentication in Microsoft Outlook Express
+                        mSmtpClient.Credentials = nc.GetCredential(mSmtpClient.Host, mSmtpClient.Port, "NTLM");
+                    }
+                    else
+                    {
+                        mSmtpClient.Credentials = new System.Net.NetworkCredential(this.mSenderUsername, this.mSenderPassword);
+                    }
+                    mSmtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                    mSmtpClient.Send(mMailMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggerUtils.LogIn(LoggerUtils.ColectExceptionMessage(ex, "At function:Send() .OpporunMail 124 rows"), LogType.ErrorLog);
+            }
+        }
+    }
+    public class SendMailInfo
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+
+    /// <summary>
+    /// 验证码邮件发送
+    /// </summary>
+    public class VerifiedMail
+    {
+        #region
+
+        /// <summary>
+        /// 邮件发送队列
+        /// </summary>
+        private static Queue<OpporunMail> _mailQueue;
+
+        private string mailserver = "smtp.qq.com";
+        private string mailserverport = "587";
+        private string user = "272665534";
+        private string password = "suylewzpzhiwcagg";
+        private string sendUser = "272665534@qq.com";
+
+
+        #endregion
+
+        #region 构造器
+        private static VerifiedMail _mailsender = null;
+        private Thread sendThread = null;
+        public static VerifiedMail Sender
+        {
+            get
+            {
+                if (_mailsender == null)
+                {
+                    _mailsender = new VerifiedMail();
+
+                }
+                return _mailsender;
+            }
+
+        }
+
+
+        public VerifiedMail()
+        {
+            _mailQueue = new Queue<OpporunMail>();
+
+            sendThread = new Thread(new ThreadStart(MailSending));
+            sendThread.Start();
+
+        }
+
+        #endregion
+
+
+
+        /// <summary>
+        /// 添加到发送队列
+        /// </summary>
+        /// <param name="info"></param>
+        public void AddSend(SendMailInfo info,IList<string> sendList)
+        {
+            OpporunMail mailinfo = new OpporunMail(mailserver, sendList, sendUser, info.Title, info.Content, user, password, mailserverport, true, false);
+            lock(_mailQueue)
+            {
+                _mailQueue.Enqueue(mailinfo);
+            }
+
+
+        }
+
+
+        private void MailSending()
+        {
+            while (true)
+            {
+                int count = 0;
+                lock (_mailQueue)
+                {
+                    count = _mailQueue.Count;
+                }
+
+                if (count > 0)
+                {
+                    lock (_mailQueue)
+                    {
+                        //出队 发送
+                        _mailQueue.Dequeue().Send();
+                        count = _mailQueue.Count;
+                    }
+                }
+                if(count == 0)
+                {
+                    Thread.Sleep(1000 * 2);
+                }
+            }
+        }
+    }
+}
---
+++ EarlySite.Core/Utils/LoggerUtils.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EarlySite.Core.Utils
+{
+    /// <summary>
+    /// Description:The common tool to save system log
+    /// Author:Haojun.Zhao
+    /// </summary>
+    public class LoggerUtils
+    {
+        public LogType type;
+
+        /// <summary>
+        /// To build a Log
+        /// </summary>
+        public static void LogIn(string content,LogType type)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Log\\";
+            string filename = "";
+            switch (type)
+            {
+                case LogType.ErrorLog:
+                    path += "ErrorLog";
+                    filename = path + "\\Error" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                    break;
+                case LogType.WorkingLog:
+                    path += "WorkingLog";
+                    filename = path + "\\Working" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                    break;
+                case LogType.SqlLog:
+                    path += "SqlErrorLog";
+                    filename = path + "\\Sql" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                    break;
+                default:
+                    path += "Log";
+                    filename = path + "\\Log" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                    break;
+            }
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            File.AppendAllText(filename, content);
+        }
+
+
+        /// <summary>
+        /// Get SQL Exception info
+        /// </summary>
+        /// <param name="ex">exception</param>
+        /// <param name="backStr">The location of exception</param>
+        /// <returns></returns>
+        public static string ColectSqlExceptionMessage(string sql,string backStr,string param = "")
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("*************************SQL异常文本*************************");
+            sb.AppendLine("【出现时间】：" + DateTime.Now.ToString());
+            sb.AppendLine("【出现地点】：" + backStr);
+            sb.AppendLine("【SQL语句】：" + sql);
+            sb.AppendLine("【SQL参数】：\r\n" + param);
+            sb.AppendLine("***************************************************************");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Get Exception message info
+        /// </summary>
+        /// <param name="ex">exception</param>
+        /// <param name="backStr">The location of exception</param>
+        /// <returns></returns>
+        public static string ColectExceptionMessage(Exception ex,string backStr)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("****************************异常文本****************************");
+            sb.AppendLine("【出现时间】：" + DateTime.Now.ToString());
+            sb.AppendLine("【出现地点】：" + backStr);
+            if (ex != null)
+            {
+                sb.AppendLine("【异常类型】：" + ex.GetType().Name);
+                sb.AppendLine("【异常信息】：" + ex.Message);
+                sb.AppendLine("【堆栈调用】：" + ex.StackTrace);
+            }
+            else
+            {
+                sb.AppendLine("【未处理异常】：" + backStr);
+            }
+            sb.AppendLine("***************************************************************");
+            return sb.ToString();
+        }
+
+    }
+
+
+    /// <summary>
+    /// The enum with log
+    /// </summary>
+    public enum LogType
+    {
+        /// <summary>
+        /// 错误日志
+        /// </summary>
+        ErrorLog = 0x00,
+        /// <summary>
+        /// 运行日志
+        /// </summary>
+        WorkingLog = 0x01,
+        /// <summary>
+        /// SQL错误日志
+        /// </summary>
+        SqlLog = 0x02
+
+    }
+}
