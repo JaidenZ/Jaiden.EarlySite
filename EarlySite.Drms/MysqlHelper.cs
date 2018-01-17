ff48be91cb7235b71867b0afcab2052f3da5ff80
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-01-17 13:38:56 +0800

    feature:加入mysqlhelper

---
+++ EarlySite.Drms/MysqlHelper.cs
@@ -0,0 +1,16 @@
+﻿using MySql;
+using MySql.Data;
+using System.Configuration;
+
+
+namespace EarlySite.Drms
+{
+    public class MysqlHelper
+    {
+        public static string mysqlConnStr = ConfigurationManager.ConnectionStrings["MySql"].ConnectionString;
+
+
+
+
+    }
+}
