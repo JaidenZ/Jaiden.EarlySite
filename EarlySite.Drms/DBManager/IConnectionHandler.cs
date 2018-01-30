00cd40e2e8b7a3ea01eef3d758337c898ed03393
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-01-30 10:14:00 +0800

    feature:增加数据库连接管理处理器

---
+++ EarlySite.Drms/DBManager/IConnectionHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace EarlySite.Drms.DBManager
+{
+    /// <summary>
+    /// 连接控制器
+    /// </summary>
+    public interface IConnectionHandler
+    {
+
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        string connectionStr { get; set; }
+
+        /// <summary>
+        /// 是否连接到数据库
+        /// </summary>
+        bool Connected { get; set; }
+
+        /// <summary>
+        /// 开启连接
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// 断开连接
+        /// </summary>
+        void Stop();
+
+    }
+}
--- EarlySite.Drms/MysqlHelper.cs
+++ EarlySite.Drms/MysqlHelper.cs
@@ -9,7 +9,26 @@
     {
         private readonly static string mysqlConnStr = ConfigurationManager.ConnectionStrings["MySql"].ConnectionString;
 
+        private static MySql.Data.MySqlClient.MySqlConnection mySqlConnection;
 
+        
+
+
+
+        public static int ExecuteNonQuery(string command)
+        {
+            int result = 0;
+            
+            using(MySql.Data.MySqlClient.MySqlCommand mySqlCommand = new MySql.Data.MySqlClient.MySqlCommand(command, mySqlConnection))
+            {
+                result = mySqlCommand.ExecuteNonQuery();
+            }
+            mySqlConnection.Close();
+
+
+
+            return result;
+        }
 
     }
 }
