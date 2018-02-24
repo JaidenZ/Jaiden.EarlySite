7f0026491db370861054e1479c1e2a7fa2fd113b
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-02-24 10:34:15 +0800

    fix:修复账户新增的SQL问题

--- EarlySite.Drms/Spefication/AccountAddSpefication.cs
+++ EarlySite.Drms/Spefication/AccountAddSpefication.cs
@@ -14,10 +14,10 @@
 
         public override string Satifasy()
         {
-            return string.Format("insert Phone,Email,SecurityCode,CreatDate,BirthdayDate,NickName,Avator,BackCorver," +
-                "Sex,Description,RequiredStatus into witch_account values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')",
+            return string.Format("insert into which_account (Phone,Email,SecurityCode,CreatDate,BirthdayDate,NickName,Avator,BackCorver," +
+                "Sex,Description,RequiredStatus) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')",
                 _account.Phone, _account.Email, _account.SecurityCode, _account.CreatDate, _account.BirthdayDate, _account.NickName,
-                _account.Avator, _account.BackCorver, _account.Sex.GetHashCode(), _account.Description, _account.RequiredStatus);
+                _account.Avator, _account.BackCorver, _account.Sex.GetHashCode(), _account.Description, _account.RequiredStatus.GetHashCode());
         }
     }
 }
