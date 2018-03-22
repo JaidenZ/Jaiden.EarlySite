6121349bfc2a242bc5fac91773a83cfc1cd17ce3
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-03-22 10:46:39 +0800

    feature:增加枚举信息

--- EarlySite.Model/Enum/DishTypeEnum.cs
+++ EarlySite.Model/Enum/DishTypeEnum.cs
@@ -1,11 +1,52 @@
 ﻿namespace EarlySite.Model.Enum
 {
     /// <summary>
+    /// 用餐时间
+    /// </summary>
+    public enum MealTime
+    {
+        /// <summary>
+        /// 所有时间段
+        /// </summary>
+        All = 0,
+        /// <summary>
+        /// 早餐
+        /// </summary>
+        BreakFast = 1,
+        /// <summary>
+        /// 午餐
+        /// </summary>
+        Lunch = 2,
+        /// <summary>
+        /// 下午茶
+        /// </summary>
+        TeaTime = 3,
+        /// <summary>
+        /// 晚餐
+        /// </summary>
+        Dinner = 4,
+        /// <summary>
+        /// 夜宵
+        /// </summary>
+        NightSnack = 5
+    }
+
+
+
+    /// <summary>
     /// 食物类型
     /// </summary>
     public enum DishType
     {
-
+        饮品甜点 = 1000,
+        火锅 = 1001,
+        自助餐 = 1002,
+        小吃快餐 = 1003,
+        日韩料理 = 1004,
+        西餐 = 1005,
+        烧烤烤肉 = 1006,
+        川菜 = 1007,
+        粤菜 = 1008
 
 
     }
