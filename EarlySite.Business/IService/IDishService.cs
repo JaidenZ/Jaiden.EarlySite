4f68686064491b0aa08fdf53652709d3a7e4ffbc
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-03-22 11:30:24 +0800

    feature:添加单品食物服务

---
+++ EarlySite.Business/IService/IDishService.cs
@@ -0,0 +1,15 @@
+﻿namespace EarlySite.Business.IService
+{
+    using Model.Show;
+    using Model.Common;
+
+    /// <summary>
+    /// 单品食物服务
+    /// </summary>
+    public class IDishService:IService
+    {
+
+
+
+    }
+}
