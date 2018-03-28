003dff7500526e22641cba460a22ad112bc4604f
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-03-28 22:08:48 +0800

    feature:完善规约

--- EarlySite.Drms/Spefication/DishSpefication/DishAddSpefication.cs
+++ EarlySite.Drms/Spefication/DishSpefication/DishAddSpefication.cs
@@ -1,7 +1,5 @@
 ﻿namespace EarlySite.Drms.Spefication
 {
-    using System;
-    using System.Collections.Generic;
     using System.Text;
     using EarlySite.Model.Database;
 
@@ -19,7 +17,7 @@
             StringBuilder sql = new StringBuilder();
             sql.AppendFormat(" insert into which_dish DishId,Name,UpdateDate,Type,TypeName,MealTime,ShopId,ShopName,Image,Description ");
             sql.Append(" values ");
-            sql.AppendFormat(" ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')", _dishInfo.DIshId, _dishInfo.Name, _dishInfo.UpdateDate, _dishInfo.Type.GetHashCode(), _dishInfo.TypeName, _dishInfo.MealTime, _dishInfo.ShopId, _dishInfo.ShopName, _dishInfo.Image, _dishInfo.Description);
+            sql.AppendFormat(" ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')", _dishInfo.DIshId, _dishInfo.Name, _dishInfo.UpdateDate.ToString("yyyy-MM-dd HH:mm:ss"), _dishInfo.Type.GetHashCode(), _dishInfo.TypeName, _dishInfo.MealTime, _dishInfo.ShopId, _dishInfo.ShopName, _dishInfo.Image, _dishInfo.Description);
             
             return sql.ToString();
         }
---
+++ EarlySite.Drms/Spefication/DishSpefication/DishUpdateSpefication.cs
@@ -0,0 +1,24 @@
+﻿namespace EarlySite.Drms.Spefication.DishSpefication
+{
+    using System.Text;
+    using EarlySite.Model.Database;
+
+    public class DishUpdateSpefication : SpeficationBase
+    {
+        private DishInfo _info;
+
+        public DishUpdateSpefication(DishInfo info)
+        {
+            _info = info;
+        }
+
+        public override string Satifasy()
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.AppendFormat("update which_dish set Name = '{0}',UpdateDate = '{1}',Type = '{2}',TypeName = '{3}',MealTime = '{4}',ShopId = '{5}',ShopName = '{6}',Image = '{7}',Description = '{8}' where DishId = '{9}'",
+                _info.Name, _info.UpdateDate.ToString("yyyy-MM-dd HH:mm:ss"), _info.Type.GetHashCode(), _info.TypeName, _info.MealTime.GetHashCode(), _info.ShopId, _info.ShopName, _info.Image, _info.Description, _info.DIshId);
+
+            return sql.ToString();
+        }
+    }
+}
