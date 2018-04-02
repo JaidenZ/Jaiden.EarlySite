4f7d344487045308e423901e8557c2ce06f16315
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-04-02 10:36:15 +0800

    feture:添加规约

---
+++ EarlySite.Drms/Spefication/DishSpefication/DishDeleteSpefication.cs
@@ -0,0 +1,20 @@
+﻿namespace EarlySite.Drms.Spefication
+{
+    /// <summary>
+    /// 单品删除规约
+    /// </summary>
+    public class DishDeleteSpefication : SpeficationBase
+    {
+        private int _id;
+
+        public DishDeleteSpefication(int dishId)
+        {
+            _id = dishId;
+        }
+
+        public override string Satifasy()
+        {
+            return string.Format(" delete from which_dish where DishId = '{0}' ", _id);
+        }
+    }
+}
