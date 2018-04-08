46fd20b8389247f219d5f117d35e55fbdf864105
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-04-08 09:33:08 +0800

    feature:完善食谱服务

--- EarlySite.Business/Constract/RecipesService.cs
+++ EarlySite.Business/Constract/RecipesService.cs
@@ -3,7 +3,11 @@
     using System;
     using System.Collections.Generic;
     using Business.IService;
+    using EarlySite.Core.Utils;
+    using EarlySite.Drms.DBManager;
+    using EarlySite.Drms.Spefication;
     using EarlySite.Model.Common;
+    using EarlySite.Model.Database;
     using EarlySite.Model.Enum;
     using EarlySite.Model.Show;
 
@@ -12,7 +16,37 @@
     {
         public Result CreatRecipes(Recipes recipes)
         {
-            throw new NotImplementedException();
+            Result result = new Result()
+            {
+                Status = true,
+                Message = "创建食谱成功"
+            };
+
+            try
+            {
+                RecipesInfo addinfo = recipes.Copy<RecipesInfo>();
+                result.Status = DBConnectionManager.Instance.Writer.Insert(new RecipesAddSpefication(addinfo).Satifasy());
+
+                if (result.Status)
+                {
+                    DBConnectionManager.Instance.Writer.Commit();
+                }
+                else
+                {
+                    DBConnectionManager.Instance.Writer.Rollback();
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+                DBConnectionManager.Instance.Writer.Rollback();
+                result.Status = false;
+                result.Message = "创建食谱出错:" + ex.Message;
+
+            }
+
+            return result;
         }
 
         public Result<IList<Recipes>> GetRecipesByPhone(long phone)
