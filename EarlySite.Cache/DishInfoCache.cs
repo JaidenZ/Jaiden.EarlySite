19112122eee3f2303896179ee47132b3ca2d87da
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-05-18 09:48:15 +0800

    feature:缓存迭代

--- EarlySite.Cache/DishInfoCache.cs
+++ EarlySite.Cache/DishInfoCache.cs
@@ -1,9 +1,28 @@
 ﻿namespace EarlySite.Cache
 {
+
+    using System;
+
     /// <summary>
     /// 单品信息缓存
     /// </summary>
     public class DishInfoCache
     {
+        /**
+         * 单品信息缓存Redis Key格式
+         * DBDish_单品编号
+         * */
+
+
+        /// <summary>
+        /// 有效时间
+        /// </summary>
+        private const int EffectiveTime = 7;
+
+        /// <summary>
+        /// 失效时间
+        /// </summary>
+        private static DateTime ExpireTime { get { return DateTime.Now.AddDays(EffectiveTime); } }
+
     }
 }
