9cb4129ebd1805b242895c0677e0c4b24a5b6298
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-05-22 09:20:04 +0800

    feature:完善单品信息缓存

--- EarlySite.Cache/DishInfoCache.cs
+++ EarlySite.Cache/DishInfoCache.cs
@@ -1,6 +1,7 @@
 ﻿namespace EarlySite.Cache
 {
-
+    using System.Collections.Generic;
+    using EarlySite.Model.Database;
     using System;
 
     /// <summary>
@@ -23,6 +24,37 @@
         /// 失效时间
         /// </summary>
         private static DateTime ExpireTime { get { return DateTime.Now.AddDays(EffectiveTime); } }
+
+        /// <summary>
+        /// 保存单品信息到缓存
+        /// </summary>
+        /// <param name="dish">单品信息</param>
+        /// <returns></returns>
+        public static bool SaveDishInfoToCache(DishInfo dish)
+        {
+
+            string key = dish.GetKeyName();
+            bool issuccess = false;
+            issuccess = Session.Current.Set(key, dish);
+            Session.Current.Expire(key, ExpireTime);
+            return issuccess;
+        }
+
+        /// <summary>
+        /// 保存单品信息到缓存
+        /// </summary>
+        /// <param name="dish">单品信息集合</param>
+        public static void SaveDishInfoToCache(IList<DishInfo> dish)
+        {
+            if(dish == null)
+            {
+                throw new ArgumentNullException("DishInfo collection is null");
+            }
+            foreach (DishInfo item in dish)
+            {
+                SaveDishInfoToCache(item);
+            }
+        }
 
     }
 }
