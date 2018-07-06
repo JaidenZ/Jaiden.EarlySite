47474f33191a24488a7b6073ae651eadb84b37ad
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-07-06 14:01:25 +0800

    feat:新增发现页面

---
+++ EarlySite.Web/Controllers/ExploreController.cs
@@ -0,0 +1,30 @@
+﻿namespace EarlySite.Web.Controllers
+{
+
+    using System;
+    using System.Web.Mvc;
+    using EarlySite.Business.IService;
+    using EarlySite.Core.DDD.Service;
+    using EarlySite.Model.Common;
+    using EarlySite.Model.Enum;
+    using EarlySite.Model.Show;
+
+    /// <summary>
+    /// 发现视图控制器
+    /// </summary>
+    public class ExploreController : BaseController
+    {
+        
+        /// <summary>
+        /// 发现主页
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //获取用户数据
+            ViewBag.Account = base.CurrentAccount;
+            return View();
+        }
+    }
+}
