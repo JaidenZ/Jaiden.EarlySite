f89be64885d03e6c043d59a308d0cfe99df2257c
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-07-09 16:47:54 +0800

    feat:添加公共组件部分

---
+++ EarlySite.Web/Controllers/ComponentController.cs
@@ -0,0 +1,27 @@
+﻿namespace EarlySite.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// 组件控制器
+    /// </summary>
+    public class ComponentController : BaseController
+    {
+
+        /// <summary>
+        /// 生成单品分享组件界面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult GenerateDishShareView()
+        {
+
+            ViewBag.Meal = 1;
+            return View();
+        }
+
+
+
+
+    }
+}
