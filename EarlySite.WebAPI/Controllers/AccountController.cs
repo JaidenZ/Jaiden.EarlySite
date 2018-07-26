327ffcf2118d5300c6effcf097ddb5c5430a6d6e
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-07-26 16:01:48 +0800

    feat:添加WEBAPI项目

---
+++ EarlySite.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using EarlySite.Business.IService;
+using EarlySite.Core;
+using EarlySite.Core.DDD.Service;
+using EarlySite.Model.Common;
+using EarlySite.Model.Show;
+
+namespace EarlySite.WebAPI.Controllers
+{
+    public class AccountController : ApiController
+    {
+        /// <summary>
+        /// 根据手机号获取账户信息
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
+        public Result<Account> GetAccountInfoByPhone(long phone)
+        {
+            return ServiceObjectContainer.Get<IAccountService>().GetAccountInfo(phone);
+        }
+
+
+    }
+}
---
+++ EarlySite.WebAPI/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using EarlySite.Core.Component;
+using EarlySite.Core.DDD.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace EarlySite.WebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //预备
+            Prepared();
+        }
+
+
+
+        protected void Prepared()
+        {
+            //获取业务服务程序集
+            Assembly businessdll = Assembly.Load("EarlySite.Business");
+            //获取业务服务程序集
+            Assembly cachedll = Assembly.Load("EarlySite.Cache");
+            //注册业务服务程序集
+            ServiceObjectContainer.Load(businessdll);
+            ServiceObjectContainer.Load(cachedll);
+
+            Thread work = Thread.CurrentThread;
+            lock (work)
+            {
+                if (WorkThreadDictionary.Get(work) == null)
+                {
+                    WorkThreadDictionary.Create(Thread.CurrentThread);
+                    //EarlySite.Cache.Session.Deployment();
+                    EarlySite.Cache.Session.DeploymentForWeb();
+                }
+            }
+
+            //加载Redis
+            //RedisClientManager.ReadOnlyHosts = new string[] { "192.168.11.103:6379" };
+            //RedisClientManager.ReadOnlyHosts = new string[] { "192.168.11.103:6379" };
+
+
+
+            //EarlySite.Cache.Session.Current.Set("test", "1111");
+
+        }
+    }
+}
