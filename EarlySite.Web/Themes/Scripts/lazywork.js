49152920cb07bf67a5cd446bc803f7edc09fc7c2
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-08-09 08:57:07 +0800

    feat:取消延迟加载,顺序加载

--- EarlySite.Web/Themes/Scripts/lazywork.js
+++ EarlySite.Web/Themes/Scripts/lazywork.js
@@ -1,11 +1,12 @@
 ﻿
 onmessage = function (event) {
-    
-    var timeout = Math.floor(Math.random() * 10 + 1) * 10;
     var id = event.data;
-    setTimeout(function () {
-        postMessage(id);
-    }, timeout);
+    postMessage(id);
+    //var timeout = Math.floor(Math.random() * 10 + 1) * 10;
+    //var id = event.data;
+    //setTimeout(function () {
+    //    postMessage(id);
+    //}, timeout);
 
     
 }
