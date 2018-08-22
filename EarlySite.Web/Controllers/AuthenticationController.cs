bcea806abe831ec0227a24351e6f45f163092f84
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-08-22 19:52:46 +0800

    feat:加入认证控制器

---
+++ EarlySite.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿namespace EarlySite.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// 第三方认证控制器
+    /// 
+    /// 预计接入 QQ Wechat Weibo 账户登录
+    /// </summary>
+    public class AuthenticationController : Controller
+    {
+
+
+
+    }
+}
