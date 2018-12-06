79fffc222a166e4776cd3ea23b1e20c1b19a17cf
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-12-06 10:53:32 +0800

    feat:收藏规约添加

---
+++ EarlySite.Drms/Spefication/FavoriteSpefication/FavoriteAddSpefication.cs
@@ -0,0 +1,45 @@
+﻿namespace EarlySite.Drms.Spefication.FavoriteSpefication
+{
+    using System.Collections.Generic;
+    using System.Text;
+    using EarlySite.Model.Database;
+    using EarlySite.Model.Enum;
+
+    /// <summary>
+    /// 收藏新增规约
+    /// </summary>
+    public class FavoriteAddSpefication : SpeficationBase
+    {
+
+        private IList<FavoriteInfo> _info;
+
+        /// <summary>
+        /// 收藏新增规约
+        /// </summary>
+        public FavoriteAddSpefication(IList<FavoriteInfo> info)
+        {
+            _info = info;
+        }
+
+        public override string Satifasy()
+        {
+
+            StringBuilder sql = new StringBuilder();
+
+            if (_info != null && _info.Count > 0)
+            {
+                sql.Append("insert into relation_favorite (Phone,FavoriteId,FavoriteType,UpdateDate) values");
+
+                for (int i = 0; i < _info.Count; i++)
+                {
+                    sql.AppendFormat("('{0}','{1}','{2}','{3}')", _info[i].Phone, _info[i].FavoriteId, _info[i].FavoriteType, _info[i].UpdateDate.ToString("yyyy-MM-dd HH:mm:ss"));
+                    if (i != _info.Count - 1)
+                    {
+                        sql.Append(" , ");
+                    }
+                }
+            }
+            return sql.ToString();
+        }
+    }
+}
---
+++ EarlySite.Drms/Spefication/FavoriteSpefication/FavoriteDeleteSpefication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarlySite.Drms.Spefication.FavoriteSpefication
+{
+    public class FavoriteDeleteSpefication : SpeficationBase
+    {
+        private long _phone;
+        private int _favoriteid;
+
+
+        public FavoriteDeleteSpefication(long phone, int favoriteid)
+        {
+            _phone = phone;
+            _favoriteid = favoriteid;
+        }
+
+        public override string Satifasy()
+        {
+            string sql = string.Format("delete from relation_favorite where Phone = '{0}' and FavoriteId = '{1}'", _phone, _favoriteid);
+            
+            return sql;
+
+        }
+    }
+}
