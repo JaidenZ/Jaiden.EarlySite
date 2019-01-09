717fa1abf2063dbf9171285190c02e4081d0cb39
Haojun.Zhao <Haojun.Zhao@icloud.com>
2019-01-09 17:24:42 +0800

    feat:提交测试接口模型

--- EarlySite.WebAPI/Controllers/AccountController.cs
+++ EarlySite.WebAPI/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using EarlySite.Core;
 using EarlySite.Core.DDD.Service;
 using EarlySite.Model.Common;
 using EarlySite.Model.Show;
+using EarlySite.WebAPI.Models;
 
 namespace EarlySite.WebAPI.Controllers
 {
@@ -21,6 +22,25 @@ namespace EarlySite.WebAPI.Controllers
             return ServiceObjectContainer.Get<IAccountService>().GetAccountInfo(phone);
         }
 
+
+
+        [HttpPost]
+        public Result<ConsoleModel> ConsoleModel()
+        {
+
+            ConsoleModel model = new Models.ConsoleModel();
+
+            model.Age = 1;
+            model.Name = "张三";
+            model.Sex = 3;
+
+            Result<ConsoleModel> result = new Result<Models.ConsoleModel>();
+            result.Status = true;
+            result.Data = model;
+
+            return result;
+
+        }
 
     }
 }
---
+++ EarlySite.WebAPI/Models/ConsoleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EarlySite.WebAPI.Models
+{
+
+
+    /// <summary>
+    /// 输出模型
+    /// </summary>
+    public class ConsoleModel
+    {
+        public int Age { get; set; }
+        
+        public int Sex { get; set; }
+
+        public string Name { get; set; }
+    }
+}
